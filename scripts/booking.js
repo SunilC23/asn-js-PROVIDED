7330f09fa5d2695cedcd1f47424b05c4189f2eec
SunilC23 <153144665+SunilC23@users.noreply.github.com>
2023-12-14 18:15:43 -0700

    Add files via upload

--- scripts/booking.js
+++ scripts/booking.js
@@ -3,38 +3,79 @@
 // Do any of these variables need to be initialized when the page is loaded? 
 // When do they need to be reset or updated?
 
+var dailyRate = 35;
+var dayCounter = 0;
 
+document.addEventListener ('DOMContentLoaded', function () {
 
 
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-
-
+function toggleDay(element) {
+    if (element.classList.contains('clicked')) {
+        element.classList.remove('clicked');
+        dayCounter--;
+    } else {
+        element.classList.add('clicked');
+        dayCounter++;
+    }
+    calculate();
+}
+var dayButtons = document.querySelectorAll('.day-selector li');
+dayButtons.forEach(function (day) {
+    day.addEventListener('click', function () {
+        toggleDay(day);
+    });
+});
 
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
-
-
-
+var clearButton = document.getElementById('clear-button');
+    if (clearButton) {
+        clearButton.addEventListener('click', function () {
+            dayButtons.forEach(function (day) {
+                day.classList.remove('clicked');
+            });
+            dayCounter = 0;
+            calculate();
+        });
+    }
 
 
 
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
+var halfButton = document.getElementById('half');
+    var fullButton = document.getElementById('full');
+    if (halfButton && fullButton) {
+        halfButton.addEventListener('click', function () {
+            dailyRate = 20;
+            halfButton.classList.add('clicked');
+            fullButton.classList.remove('clicked');
+            calculate();
+        });
 
+        fullButton.addEventListener('click', function () {
+            dailyRate = 35;
+            fullButton.classList.add('clicked');
+            halfButton.classList.remove('clicked');
 
-
-// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
-
-
-
+            calculate();
+        });
+    }
 
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
-
-
+function calculate() {
+    var calculatedCost = dailyRate * dayCounter;
+    var calculatedCostElement = document.getElementById('calculated-cost');
+    if (calculatedCostElement) {
+        calculatedCostElement.innerHTML = calculatedCost;
+    }
+}
+});
--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,19 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+document.addEventListener('DOMContentLoaded', function () {
+    var submitButton = document.getElementById('submit-button');
+
+    if (submitButton) {
+        submitButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            var contactPage = document.getElementById('contact-page');
+            if (contactPage) {
+                contactPage.innerHTML = '<p style="font-size: 24px;">Thank you for your message</p>';
+            } else {
+                console.error('Contact page element not found');
+            }
+        });
+    }
+});
+
